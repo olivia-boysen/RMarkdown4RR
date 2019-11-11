0b60d1902328540416428bdf6e0d2ef74e559034
TanjaKec <tanja.kec@virginmedia.com>
2019-11-11 08:53:08 +0100

    required packages

---
+++ 00_pckg_load.R
@@ -0,0 +1,11 @@
+install.packages("rmarkdown")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("DT")
+install.packages("tidyverse")
+install.packages("here")
+install.packages("gapminder")
+install.packages("knitr")
+install.packages("rticles")
+
+
